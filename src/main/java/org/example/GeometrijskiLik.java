6c9712121f8b084cdad15018bfacb8523433ae27
Sven <svengavrano@gmail.com>
2023-08-17 16:19:05 +0200

    Commit 1 do 3!

--- src/main/java/org/example/GeometrijskiLik.java
+++ src/main/java/org/example/GeometrijskiLik.java
@@ -2,4 +2,26 @@ package org.example;
 
 public class GeometrijskiLik {
 
+    GeometrijskiLik(String boja){
+        this.boja = boja;
+        povrsina = 0.0;
+    }
+    GeometrijskiLik(double povrsina){
+        boja = null;
+        this.povrsina = povrsina;
+    }
+    GeometrijskiLik(String boja, double povrsina){
+        this.boja = boja;
+        this.povrsina = povrsina;
+    }
+    private String boja;
+    private double povrsina;
+
+    public String getBoja() {
+        return boja;
+    }
+
+    public double getPovrsina(){
+        return povrsina;
+    }
 }
